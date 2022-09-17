f9b7ba4098e5f5dab8300ecaea009b030f627d1e
kfloyd-cyber <77768567+kfloyd-cyber@users.noreply.github.com>
2022-09-17 13:22:41 -0500

    Project Initiation

---
+++ CommerceBankApp/CommerceBankApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CommerceBankApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
