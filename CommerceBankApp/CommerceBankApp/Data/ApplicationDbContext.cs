bbc7033a0ad6c77839d2c945181bddce95ca5851
Balladaris <77756198+Balladaris@users.noreply.github.com>
2022-09-24 14:24:11 -0500

    update ApplicationDbContext to inherit from AuthDbContext

--- CommerceBankApp/CommerceBankApp/Data/ApplicationDbContext.cs
+++ CommerceBankApp/CommerceBankApp/Data/ApplicationDbContext.cs
@@ -1,15 +1,25 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using CommerceBankApp.Areas.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using CommerceBankApp.Models;
 
 namespace CommerceBankApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : AuthDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+        
         public DbSet<CommerceBankApp.Models.DonationType> DonationType { get; set; }
         public DbSet<CommerceBankApp.Models.Account> Account { get; set; }
         public DbSet<CommerceBankApp.Models.Donor> Donor { get; set; }
